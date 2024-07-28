e73a133606e6e27348637ff87c40581b6ab4ad43
Additya Akash Mishra <77526069+VictoriousMango@users.noreply.github.com>
2024-07-28 17:48:58 +0530

    Time: 33 ms (74.2%), Space: 16.5 MB (86.01%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,11 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         brackets = list()
+        checker = {
+            "(" : ")",
+            "{" : "}",
+            "[" : "]"
+        }
         for i in s:
-            if len(brackets):
-                if (brackets[-1] == "(" and i == ")") \
-                    or (brackets[-1] == "{" and i == "}") \
-                    or (brackets[-1] == "[" and i == "]"):
+            if len(brackets) and brackets[-1] in checker.keys():
+                if checker[brackets[-1]] == i:
                     brackets.pop()
                 else:
                     brackets.append(i)
