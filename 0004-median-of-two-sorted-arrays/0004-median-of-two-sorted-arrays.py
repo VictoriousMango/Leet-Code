c89da7f417c8121ad2cba87613df92926e9a4cf0
Additya Akash Mishra <77526069+VictoriousMango@users.noreply.github.com>
2024-08-02 21:03:44 +0530

    Time: 70 ms (95.19%), Space: 16.8 MB (90.22%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -3,6 +3,8 @@ class Solution:
         new_index, index1, index2 = 0, 0, 0
         new_list = list()
         while (index1 < len(nums1)) and (index2 < len(nums2)) :
+            if new_index > (len(nums1) + len(nums2))//2:
+                break 
             if nums1[index1] < nums2[index2]:
                 new_list.append(nums1[index1])
                 new_index, index1 = new_index+1, index1+1
@@ -10,11 +12,16 @@ class Solution:
                 new_list.append(nums2[index2])
                 new_index, index2 = new_index+1, index2+1
         while (index1 < len(nums1)):
+            if new_index > (len(nums1) + len(nums2))//2:
+                break 
             new_list.append(nums1[index1])
             new_index, index1 = new_index+1, index1+1
         while (index2 < len(nums2)):
+            if new_index > (len(nums1) + len(nums2))//2:
+                break 
             new_list.append(nums2[index2])
             new_index, index2 = new_index+1, index2+1
         print(new_list)
-        return new_list[((len(new_list)+1)//2)-1] if len(new_list)%2==1 else (new_list[len(new_list)//2] + new_list[(len(new_list)//2) - 1])/2
+
+        return new_list[-1] if (len(nums1) + len(nums2))%2==1 else (new_list[-1] + new_list[-2])/2
         
