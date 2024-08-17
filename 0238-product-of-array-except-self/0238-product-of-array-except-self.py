d250825f13afba9b3465af10ee6ec602cf91c404
Additya Akash Mishra <77526069+VictoriousMango@users.noreply.github.com>
2024-08-17 11:36:59 +0530

    Time: 286 ms (17.97%), Space: 25.7 MB (81.19%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -14,25 +14,5 @@ class Solution:
             if i < len(nums)-1:
                 postfix *= nums[i+1]
             answer[i] *= postfix
-        return answer
-        # index_pre = 0
-        # index_post = len(nums)-1
-        # prefix = [nums[index_pre]]
-        # postfix = [nums[index_post]]
-        # index_pre, index_post = index_pre+1, index_post-1
-        # while index_pre < len(nums):
-        #     prefix.append(nums[index_pre] * prefix[index_pre-1])
-        #     postfix.append(nums[index_post] * postfix[index_pre-1])
-        #     index_pre+=1
-        #     index_post-=1
-        # print(prefix)
-        # postfix = postfix[::-1]
-        # print(postfix)
-        # return [prefix[i]*postfix[i] for i in range(len(nums))]
-            
-
-            
-
-
-        return []
+        return answer       
         
