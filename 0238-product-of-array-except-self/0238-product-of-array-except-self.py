92ed0bb5535372a7a2e7444da3cf0a23cdb063c8
Additya Akash Mishra <77526069+VictoriousMango@users.noreply.github.com>
2024-08-17 11:37:52 +0530

    Time: 276 ms (35.43%), Space: 25.7 MB (81.19%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,13 +1,13 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:   
-        answer = [1 for i in range(len(nums))]
+        answer = list()
         prefix = 1
         postfix = 1
         # Multiplying Left Sub-string
         for i in range(len(nums)):
             if i >= 1:
                 prefix *= nums[i-1]
-            answer[i] *= prefix
+            answer.append(prefix)
         
         # Multiplying Right Sub-string
         for i in range(len(nums)-1, -1, -1):
